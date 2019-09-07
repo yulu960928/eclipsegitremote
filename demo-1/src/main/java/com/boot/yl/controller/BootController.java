ee89d715ca38187b1b4794b2b7d50d427949a5a4
yulu <1479913245@qq.com>
2019-09-07 10:13:09 +0800

    修改了//hello

--- demo-1/src/main/java/com/boot/yl/controller/BootController.java
+++ demo-1/src/main/java/com/boot/yl/controller/BootController.java
@@ -1,5 +1,6 @@
 package com.boot.yl.controller;
 
+//hello
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
